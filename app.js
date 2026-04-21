93c3432b88b0a250225c9d0b4015577519e9356a
lalitha21012004 <lochlalitha67@gmail.com>
2026-04-21 12:48:04 +0530

    Deploying frontend

---
+++ app.js
@@ -0,0 +1,43 @@
+const API = 'https://task-manager-backend-ozbe.onrender.com/tasks';
+
+//Load Tasks
+async function loadTasks(){
+    let res=await axios.get(API);
+    let tasks=res.data;
+    let list=document.getElementById('tasklist');
+    list.innerHTML='';
+    tasks.forEach(t=>{
+        let li=document.createElement("li");
+        li.innerHTML=`
+        <span class="${t.completed ? 'completed': ''}"> ${t.title}</span>
+        <button onclick="toggleTask('${t._id}',${!t.completed})">${t.completed ? 'Undo' : 'Complete'}</button>
+        <button onclick="deleteTask('${t._id}')">Delete</button>
+        `;
+        list.appendChild(li);
+
+
+    });
+}
+//Add task
+async function addTask(){
+    let input=document.getElementById('taskinput');
+    if (!input.value) return;
+    await axios.post(API,{ title:input.value});
+    input.value="";
+    loadTasks();
+}
+
+//Toggle task
+async function toggleTask(id,completed){
+    await axios.put(`${API}/${id}`,{completed});
+    loadTasks();
+}
+
+//Delete task
+async function deleteTask(id){
+    await axios.delete(`${API}/${id}`);
+    loadTasks();
+}
+
+//Initial load
+loadTasks();
